994c6455f9b75c3d5035bb1bf9cf48e655eaa09b
이재영 <ldc1000@naver.com>
2024-12-23 21:16:40 +0900

    🎉 Pokedex 프로젝트 시작

---
+++ PokedexTests/PokedexTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexTests.swift
+//  PokedexTests
+//
+//  Created by t2023-m0072 on 12/23/24.
+//
+
+import Testing
+@testable import Pokedex
+
+struct PokedexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
